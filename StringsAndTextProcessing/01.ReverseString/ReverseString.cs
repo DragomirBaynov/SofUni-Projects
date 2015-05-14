0eb679cf8ee658540ad4e33f5a7e843e2df3939a
wandererSturm <wanderer.sturm@gmail.com>
2015-05-14 14:11:43 +0300

    add StringAndTextProccessing

---
+++ StringsAndTextProcessing/01.ReverseString/ReverseString.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+class ReverseString
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        StringBuilder sb = new StringBuilder();
+        for (int i = input.Length - 1; i >= 0; i--)
+        {
+            sb.Append(input[i]);
+        }
+
+        Console.WriteLine(sb);
+    }
+}
+
---
+++ StringsAndTextProcessing/02.StringLength/StringLength.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class StringLength
+{
+    static void Main(string[] args)
+    {
+        string input = Console.ReadLine();
+        int length = input.Length;
+        string result = "";    
+        if (length < 20)
+        {
+            
+            result = input.PadRight(20,'*');
+            Console.WriteLine(result);
+        }
+        if (length>20)
+        {
+            Console.WriteLine(input.Substring(0,20));
+        }
+    }
+}
+
